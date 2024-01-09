416b716830969b1e66d22067630ab37b0611726d
たつぞう <tatsuzoaraki@gmail.com>
2024-01-10 00:10:02 +0900

    Chapter 16: Adding Metadata

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from 'next';
 import { fetchCustomers } from '@/app/lib/data';
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
- 
+
+export const metadata: Metadata = {
+    title: 'Create Invoice',
+};
+
 export default async function Page() {
   const customers = await fetchCustomers();
  
